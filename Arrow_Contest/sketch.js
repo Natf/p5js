561d33111decddb408e1a9a4282237378835ccef
Nathan Frend <theniffin@gmail.com>
2022-01-18 23:27:14 +0900

    Mod: arrow contest text

--- Arrow_Contest/sketch.js
+++ Arrow_Contest/sketch.js
@@ -1,58 +1,64 @@
 var totalGames = 0;
 var redWins = 0;
 var greenWins = 0;
+var runs = 0;
+var maxRuns = 1000;
+var drawOnce = false;
 
 
 function setup() {
-  createCanvas(400, 400);
-  frameRate(10);
+    createCanvas(400, 100);
+    frameRate(10);
 }
 
 function draw() {
-  background(220);
-  
-  for (let a = 0; a < 100; a++) {
-    if (arrowTournament() === "redwin") {
-      redWins ++;
-    } else {
-      greenWins++;
+    background(220);
+    runs++;
+    if (runs > maxRuns) {
+        noLoop();
     }
-  }
-  
-  console.log("Red wins: " + redWins +" avg("+(redWins/totalGames)+")");
-  
-  console.log("Green wins: " + greenWins +" avg("+(greenWins/totalGames)+")");
+
+    for (let a = 0; a < 100; a++) {
+        if (arrowTournament() === "redwin") {
+            redWins++;
+        } else {
+            greenWins++;
+        }
+    }
+    textSize(10);
+    text("Red wins: " + redWins + " avg(" + (redWins / totalGames) + ")", 10, 30);
+    text("Green wins: " + greenWins + " avg(" + (greenWins / totalGames) + ")", 10, 60);
 }
 
 function arrowTournament() {
-  totalGames++;
-  let winner = null;
-  while (winner === null) {
-    let greenScore = 24;
-    let redScore = 0;
-    
-    for (let i =0; i < 3; i++) {
-      switch(Math.floor(random(3))) {
-         case 0:
-             redScore +=10;
-          break;
-          case 1:
-             redScore +=9;
-          break;
-          case 2:
-             redScore +=5;
-          break;
-          default:
-          break;
-      }
+    totalGames++;
+    let winner = null;
+    while (winner === null) {
+        let greenScore = 24;
+        let redScore = 0;
+
+        for (let i = 0; i < 3; i++) {
+            switch (Math.floor(random(3))) {
+                case 0:
+                    redScore += 10;
+                    break;
+                case 1:
+                    redScore += 9;
+                    break;
+                case 2:
+                    redScore += 5;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (greenScore > redScore) {
+            winner = "greenwin";
+        } else if (redScore > greenScore) {
+            winner = "redwin";
+        }
     }
-    
-    if (greenScore > redScore) {
-      winner = "greenwin";
-    } else if (redScore > greenScore) {
-      winner = "redwin";
-    }
-  }
-  
-  return winner;
+
+    return winner;
 }
